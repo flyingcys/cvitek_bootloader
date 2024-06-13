10a813eb4edcf9cee6956b418c13d6a0a2a37178
Ghazigq <ghazi.gq@hotmail.com>
2024-06-13 03:35:57 +0000

    !1 增加LicheeRV nano板子支持 * add: board licheerv nano

---
+++ device/licheerv-nano/boardconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export MV_BOARD=licheerv-nano
+export MV_BOARD_CPU=cv1812cp
+export MV_VENDOR=lichee
+export MV_BUILD_ENV=milkvsetup.sh
+export MV_BOARD_LINK=cv1812cp_licheerv_nano_sd
+
